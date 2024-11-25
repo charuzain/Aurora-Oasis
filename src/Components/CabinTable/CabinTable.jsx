706aadfce2778bb094e1d9625718bdf730b1c059
charuzain <charuabhizain@gmail.com>
2024-11-24 22:36:21 -0500

    Add & Style cabin header component

---
+++ src/Components/CabinTable/CabinTable.jsx
@@ -0,0 +1,20 @@
+import './CabinTable.scss'
+const CabinTable = () => {
+  return (
+    <>
+      <div className="header">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </div>
+      
+      {/* <CabinTableHeader />
+      <CabinRow/> */}
+    </>
+  )
+}
+
+export default CabinTable
