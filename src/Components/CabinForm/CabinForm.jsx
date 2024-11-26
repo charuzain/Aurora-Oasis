2b89ab84e8d50454a1e9ab03aa79ff1102e5e46e
charuzain <charuabhizain@gmail.com>
2024-11-26 10:56:04 -0500

    Add form validation

--- src/Components/CabinForm/CabinForm.jsx
+++ src/Components/CabinForm/CabinForm.jsx
@@ -2,29 +2,59 @@ import { useForm } from 'react-hook-form';
 import './CabinForm.scss';
 
 const CabinForm = () => {
-  const { register, handleSubmit } = useForm()
-  const submitHandler = data => console.log(data)
+  const { register, handleSubmit, formState } = useForm();
+  const submitHandler = (data) => console.log(data);
+  const error = formState.errors;
+  // const onErrors = (errors) => console.error(errors);
   return (
     <form onSubmit={handleSubmit(submitHandler)}>
       <div>
         <label htmlFor="name">Cabin name</label>
-        <input type="text" id="name" {...register('name')} />
+        <input
+          type="text"
+          id="name"
+          {...register('name', {
+            required: 'Name is required',
+          })}
+        />
+        {error?.name && error.name.message}
       </div>
       <div>
         <label htmlFor="maxCapacity">Maximum capacity</label>
-        <input type="number" id="maxCapacity" {...register('maxCapacity')} />
+        <input
+          type="number"
+          id="maxCapacity"
+          {...register('maxCapacity', { required: 'Max capacity is required' })}
+        />
+        {error?.maxCapacity && error.maxCapacity.message}
       </div>
       <div>
         <label htmlFor="regularPrice">Regular price</label>
-        <input type="number" id="regularPrice" {...register('regularPrice')} />
+        <input
+          type="number"
+          id="regularPrice"
+          {...register('regularPrice', {
+            required: 'Regular price is required',
+          })}
+        />
+        {error?.regularPrice && error.regularPrice.message}
       </div>
       <div>
         <label htmlFor="discount">Discount</label>
-        <input type="number" id="discount" {...register('discount')} />
+        <input
+          type="number"
+          id="discount"
+          {...register('discount', { required: 'Discount is required' })}
+        />
+        {error?.discount && error.discount.message}
       </div>
       <div>
         <label htmlFor="description">Description</label>
-        <textarea id="description" {...register('description')}></textarea>
+        <textarea
+          id="description"
+          {...register('description', { required: 'Description is required' })}
+        ></textarea>
+        {error?.description && error.description.message}
       </div>
       <div>
         <label htmlFor="">Cabin photo</label>
@@ -36,6 +66,6 @@ const CabinForm = () => {
       </div>
     </form>
   );
-}
+};
 
-export default CabinForm
+export default CabinForm;
